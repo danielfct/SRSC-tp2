f490384b046c3a943ab17188ca2085fcc2b378cb
dpimenta <d.pimenta@campus.fct.unl.pt>
2018-06-08 17:01:57 +0100

    Initial commit

---
+++ src/tpm/GOS_TPM.java
@@ -0,0 +1,11 @@
+package tpm;
+
+public class GOS_TPM {
+
+	public static void main(String[] args) {
+		// protocolo de atestação:
+		// receber pedidos de atestação - correr comando ps em java e fazer hash dos processos a correr
+		// assinar tudo e enviar para o cliente
+	}
+	
+}
---
+++ src/tpm/VMS_TPM.java
@@ -0,0 +1,9 @@
+package tpm;
+
+public class VMS_TPM {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
